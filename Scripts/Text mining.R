b1ddd667a00521d44f38b5ba9062be36bcb06c56
Juan Andrés Silva Lobelo <159551207+Jsilval2024@users.noreply.github.com>
2024-05-01 20:00:13 -0500

    Text mining FINALIZADO

--- Scripts/Text mining.R
+++ Scripts/Text mining.R
@@ -152,7 +152,7 @@ conjunto = c("condominio", "condominios", "condonimio", "conjnto", "conjun",
              "conjuno", "conjunta", "conjunto", "conjuntocerradoubicada", 
              "conjuntoclub", "conjuntode", "conjuntos", "conjuto" ,
              "conjuunto", "cunjunto", "depositoconjunto", "depositocortinasconjunto", "onjunto", "elconjunto")
-gym = c("congimnasio", )
+gym = c("congimnasio", 'gimnasio', 'gym')
 cowork = c("cowork", "coworkin", "coworking", "coworkins")
 
 ### Mapa
@@ -190,3 +190,141 @@ pingpong = c("pingpong", "pinpog", "pinpon", "pinpong")
 pintado = c("pintada", "pintadas", "pintado")
 verde = c("paque", "paqrue", "paques", "parke", "parket", "parkets", "parkwey", "parqs", "parqu", "parque", "parquebarrios", "parquecito", "parquede", "parqueinfantil", "parques", "parquesbarrios", "parqus", "parqie", "paruqe", "paruqes", "parway", "pasamano", "pasamanos", "patinar", "patines", "patineta", "patinetas", "entreparques", 
           'arbol', 'arboleda', 'arboledajunto', 'arboles', 'arbolizada', 'arbolizadas','arborizacia', 'arborizacin', 'arborizacion', 'arborizada', 'arborizadas','arborizado', 'arborizados', 'arobicos', "bosque", "bosques")
+
+# Creacion de variables a partir de la descripcion:
+texto$desc_corregido_lemma <- lapply(texto$desc_corregido_lemma, unlist)
+
+texto$ascensor = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% ascensor), 1, 0)})
+
+texto$agente = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% agente), 1, 0)})
+
+texto$alfombra = lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% alfombra), 1, 0)})
+
+texto$deposito = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% deposito), 1, 0)})
+
+texto$amoblado = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% amoblado), 1, 0)})
+
+texto$antiguo = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% antiguo), 1, 0)})
+
+texto$antirruido = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% antirruido), 1, 0)})
+
+texto$antisismo = lapply(texto$desc_corregido_lemma,
+                          function(x) {ifelse(any(x %in% antisismo), 1, 0)})
+
+texto$apartaestudio = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% apartaestudio), 1, 0)})
+
+texto$hotel = lapply(texto$desc_corregido_lemma,
+                             function(x) {ifelse(any(x %in% hotel), 1, 0)})
+
+texto$duplex = lapply(texto$desc_corregido_lemma,
+                     function(x) {ifelse(any(x %in% duplex), 1, 0)})
+
+texto$balcon = lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% balcon), 1, 0)})
+
+texto$bbq = lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% bbq), 1, 0)})
+
+texto$billar = lapply(texto$desc_corregido_lemma,
+                   function(x) {ifelse(any(x %in% billar), 1, 0)})
+
+texto$inteligente = lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% inteligente), 1, 0)})
+
+texto$blindaje = lapply(texto$desc_corregido_lemma,
+                           function(x) {ifelse(any(x %in% blindaje), 1, 0)})
+
+texto$calefaccion = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% calefaccion), 1, 0)})
+
+texto$canchas = lapply(texto$desc_corregido_lemma,
+                           function(x) {ifelse(any(x %in% canchas), 1, 0)})
+
+texto$seguridad = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% seguridad), 1, 0)})
+
+texto$clubhouse = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% clubhouse), 1, 0)})
+
+texto$conjunto = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% conjunto), 1, 0)})
+
+texto$gym = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% gym), 1, 0)})
+
+texto$cowork = lapply(texto$desc_corregido_lemma,
+                   function(x) {ifelse(any(x %in% cowork), 1, 0)})
+
+texto$drywall = lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% drywall), 1, 0)})
+
+texto$edificio = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% edificio), 1, 0)})
+
+texto$lavanderia = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% lavanderia), 1, 0)})
+
+texto$embargos = lapply(texto$desc_corregido_lemma,
+                          function(x) {ifelse(any(x %in% embargos), 1, 0)})
+
+texto$enfermeria = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% enfermeria), 1, 0)})
+
+texto$escaleras = lapply(texto$desc_corregido_lemma,
+                          function(x) {ifelse(any(x %in% escaleras), 1, 0)})
+
+texto$nuevo = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% nuevo), 1, 0)})
+
+texto$obra= lapply(texto$desc_corregido_lemma,
+                     function(x) {ifelse(any(x %in% obra), 1, 0)})
+
+texto$oficina= lapply(texto$desc_corregido_lemma,
+                   function(x) {ifelse(any(x %in% oficina), 1, 0)})
+
+texto$oscuro= lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% oscuro), 1, 0)})
+
+texto$parqueadero= lapply(texto$desc_corregido_lemma,
+                     function(x) {ifelse(any(x %in% parqueadero), 1, 0)})
+
+texto$paisaje= lapply(texto$desc_corregido_lemma,
+                          function(x) {ifelse(any(x %in% paisaje), 1, 0)})
+
+texto$panoramico= lapply(texto$desc_corregido_lemma,
+                      function(x) {ifelse(any(x %in% panoramico), 1, 0)})
+
+texto$patio = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% patio), 1, 0)})
+
+texto$pavimentado = lapply(texto$desc_corregido_lemma,
+                     function(x) {ifelse(any(x %in% pavimentado), 1, 0)})
+
+texto$penthouse = lapply(texto$desc_corregido_lemma,
+                           function(x) {ifelse(any(x %in% penthouse), 1, 0)})
+
+texto$pequeño = lapply(texto$desc_corregido_lemma,
+                         function(x) {ifelse(any(x %in% pequeño), 1, 0)})
+
+texto$perro = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% perro), 1, 0)})
+
+texto$piscina = lapply(texto$desc_corregido_lemma,
+                     function(x) {ifelse(any(x %in% piscina), 1, 0)})
+
+texto$pingpong = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% pingpong), 1, 0)})
+
+texto$pintado = lapply(texto$desc_corregido_lemma,
+                        function(x) {ifelse(any(x %in% pintado), 1, 0)})
+
+texto$verde = lapply(texto$desc_corregido_lemma,
+                       function(x) {ifelse(any(x %in% verde), 1, 0)})
