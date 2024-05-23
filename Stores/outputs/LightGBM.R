0761b1dc07e486cf6cd8742035c82535a4fbc192
Juan Andrés Silva Lobelo <159551207+Jsilval2024@users.noreply.github.com>
2024-05-23 17:15:02 -0500

    Modelitos

--- Stores/outputs/LightGBM.R
+++ Stores/outputs/LightGBM.R
@@ -12,6 +12,8 @@ require(raster)
 require(stringr)
 require(tidymodels)
 require(spatialsample)
+require(lightgbm)
+require(bonsai)
 
 # Importamos datos de entrenamiento y pruebas:
 train = readRDS('train_modelos.rds')
@@ -37,18 +39,18 @@ rec_1 <- recipe(ln_price ~ . , data = train) %>%
   step_normalize(all_predictors())  # normaliza los predictores. 
 
 ### creamos specificación
-XGBreg <- boost_tree(trees = tune(),
-                     tree_depth = 4, 
-                     min_n = 25,
+lightgbm <- boost_tree(trees = tune(),
+                     tree_depth = 7, 
+                     min_n = tune(),
                      loss_reduction = 0,
-                     sample_size = 1, 
                      mtry = tune(),
-                     learn_rate = .1) %>%
-  set_engine("xgboost") %>%
+                     learn_rate = .05) %>%
+  set_engine("lightgbm") %>%
   set_mode("regression")
 
-grid <- expand.grid(trees = c(600),
-                    mtry = c(50, 55, 60))
+grid <- expand.grid(trees = c(570, 572, 574, 576, 580, 582, 584, 586, 588),
+                    min_n = c(26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39),
+                    mtry = c(31, 32))
 
 ### Configuramos flujo de trabajo
 
@@ -56,42 +58,43 @@ workflow <- workflow() %>%
   # Agregar la receta de preprocesamiento de datos. En este caso la receta 1
   add_recipe(rec_1) %>%
   # Agregar la especificación del modelo de regresión Elastic Net
-  add_model(XGBreg)
+  add_model(lightgbm)
 
 ### configuramos modelos
 set.seed(10001)
 
-tune_xgb <- tune_grid(
+tune_lightgbm <- tune_grid(
   workflow,         # El flujo de trabajo que contiene: receta y especificación del modelo
   resamples = block_folds,  # Folds de validación cruzada espacial
   metrics = metric_set(mae),
   grid = grid)
 
 ### ver resultado
-collect_metrics(tune_xgb)
+collect_metrics(tune_lightgbm)
 
 ### Mejor metrica 
-best_tune<- select_best(tune_xgb, metric = "mae")
+best_tune<- select_best(tune_lightgbm, metric = "mae")
 
 ###Finalizamos flujo
-xgb_final <- finalize_workflow(workflow, best_tune)
-xgb_final_fit <- fit(xgb_final, data = train)
+lightgbm_final <- finalize_workflow(workflow, best_tune)
+lightgbm_final_fit <- fit(lightgbm_final, data = train)
 
 # Predicciones:
-predict_xgb <- test %>% 
-  mutate(lnprice = predict(xgb_final_fit, new_data = test, type = "raw")    ## predicted class labels
+predict_lightgbm <- test %>% 
+  mutate(lnprice = predict(lightgbm_final_fit, new_data = test, type = 'numeric')    ## predicted class labels
   )  %>% select(property_id,lnprice)
 
 # Retornar el precio:
-predict_xgb = predict_xgb %>% 
+predict_lightgbm = predict_lightgbm %>% 
   mutate(price = exp(lnprice)) %>% select(property_id,price)
 
-###export submission
-export(predict_xgb, 'XGBreg.csv')
-
 # redondear el precio:
-xgb_round = predict_xgb %>% 
+lightgbm_floor = predict_lightgbm %>% 
+  mutate(price = floor(price/100000)*100000) %>% select(property_id,price)
+lightgbm_round = predict_lightgbm %>% 
   mutate(price = round(price, -5)) %>% select(property_id,price)
+lightgbm_round$price = unlist(lightgbm_round$price)
+lightgbm_floor$price = unlist(lightgbm_floor$price)
 
-export(xgb_round, 'XGBreg_3_round.csv')
-
+export(lightgbm_round, 'lightgbm_round_3.csv')
+export(lightgbm_floor, 'lightgbm_floor_3.csv')
